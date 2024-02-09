ace15353c6ca0667875b0c1427aba88a5a417102
aldimahardiansyah <aldimdiansyah@gmail.com>
2024-02-09 19:53:05 +0700

    add animation out

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,66 @@
-const objects = [document.getElementById('section2_logo'),
-document.getElementById('section2_desc'),
-document.getElementById('earth_image'),
-document.getElementById('earth_image2'),
-document.getElementById('earth_desc'),
-document.getElementById('comunities_image'),
-document.getElementById('comunities_desc'),
-document.getElementById('partner_logo1'),
-document.getElementById('partner_logo2')];
+const section2_logo = document.getElementById('section2_logo');
+const section2_desc = document.getElementById('section2_desc');
+const earth_image = document.getElementById('earth_image');
+const earth_image2 = document.getElementById('earth_image2');
+const earth_desc = document.getElementById('earth_desc');
+const comunities_image = document.getElementById('comunities_image');
+const comunities_desc = document.getElementById('comunities_desc');
+const partner_logo1 = document.getElementById('partner_logo1');
+const partner_logo2 = document.getElementById('partner_logo2');
+
+const animations = {
+    section2_logo: 'fadeInLeft',
+    section2_desc: 'fadeInRight',
+    earth_image: 'fadeInRight',
+    earth_image2: 'fadeInRight',
+    earth_desc: 'fadeInLeft',
+    comunities_image: 'fadeInLeft',
+    comunities_desc: 'fadeInRight',
+    partner_logo1: 'fadeInUp',
+    partner_logo2: 'fadeInUp',
+}
+
+const animations_out = {
+    section2_logo: 'fadeOutLeft',
+    section2_desc: 'fadeOutRight',
+    earth_image: 'fadeOutRight',
+    earth_image2: 'fadeOutRight',
+    earth_desc: 'fadeOutLeft',
+    comunities_image: 'fadeOutLeft',
+    comunities_desc: 'fadeOutRight',
+    partner_logo1: 'fadeOutUp',
+    partner_logo2: 'fadeOutUp',
+}
 
 function checkScroll() {
-    objects.forEach(function (object) {
-        let objectPosition = object.getBoundingClientRect();
+    // Looping semua elemen yang akan dianimasikan
+    for (const elementId in animations) {
+        if (animations.hasOwnProperty(elementId)) {
+            const element = document.getElementById(elementId);
+            const animationIn = animations[elementId];
+            const animationOut = animations_out[elementId];
 
-        // Cek apakah objek dalam viewport
-        if (objectPosition.top < window.innerHeight && objectPosition.bottom >= 0) {
-            object.classList.add('animate__animated'); // Tambahkan kelas animate untuk memulai animasi
-            object.classList.add('animate__fadeInDown'); // Tambahkan kelas animate untuk memulai animasi
-        } else {
-            object.classList.remove('animate__animated'); // Hapus kelas animate untuk menghentikan animasi
-            object.classList.remove('animate__fadeInDown'); // Hapus kelas animate untuk menghentikan animasi
+            // mendapatkan viewport elemen
+            let elementPosition = element.getBoundingClientRect();
+
+            // Cek apakah elemen berada dalam viewport
+            if (elementPosition.top < window.innerHeight && elementPosition.bottom >= 0) {
+                element.classList.remove(`animate__animated`); // Hapus kelas animasi yang diinginkan
+                element.classList.remove(`animate__${animationIn}`); // Hapus kelas animasi yang diinginkan
+
+                element.classList.add('animate__animated'); // Tambahkan base class animate untuk memulai animasi
+                element.classList.add(`animate__${animationIn}`); // Tambahkan kelas animasi yang diinginkan
+            }
+
+            // Cek apakah elemen berada dalam viewport
+            if (elementPosition.top < window.innerHeight && elementPosition.top <= 0) {
+                {
+                    element.classList.remove(`animate__${animationIn}`); // Hapus kelas animasi yang diinginkan
+                    element.classList.add(`animate__${animationOut}`); // Tambahkan kelas animasi yang diinginkan
+                }
+            } else {
+                element.classList.remove(`animate__${animationOut}`); // Hapus kelas animasi yang diinginkan
+            }
         }
-    });
+    }
 }
